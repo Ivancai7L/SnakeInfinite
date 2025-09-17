ac739c46be450b77cf0779215eea01383ffda8da
Ivan Langlois <ivanlangloisila@gmail.com>
2025-09-17 16:50:24 -0300

    Primer commit: estructura inicial del proyecto Snake

--- core/src/main/java/mi/proyecto/Frutas.java
+++ core/src/main/java/mi/proyecto/Frutas.java
@@ -2,9 +2,22 @@ package mi.proyecto;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 
 public class Frutas {
     private Texture texture;
-    private float x, y;
+    private int x, y;
+
+    public Frutas() {
+        texture = new Texture("Fruta.png"); // tu imagen de fruta
+        x = 200;
+        y = 200;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, x, y);
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
 }
--- core/src/main/java/mi/proyecto/Snake.java
+++ core/src/main/java/mi/proyecto/Snake.java
@@ -1,4 +1,38 @@
 package mi.proyecto;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Color;
+
 public class Snake {
+    private float x, y;
+    private float size;
+    private ShapeRenderer shapeRenderer;
+
+    public Snake() {
+        x = 100;
+        y = 100;
+        size = 20;
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    public void update() {
+        // por ahora la serpiente no se mueve, después agregamos controles
+    }
+
+    public void draw(SpriteBatch batch) {
+        // Como ShapeRenderer no usa SpriteBatch, lo dibujamos aparte
+        batch.end();
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.GREEN);
+        shapeRenderer.rect(x, y, size, size);
+        shapeRenderer.end();
+
+        batch.begin();
+    }
+
+    public void dispose() {
+        shapeRenderer.dispose();
+    }
 }
